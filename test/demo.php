1df52f050d130d06d3e4fa73a793d3a6a2fd6f72
odan <odan@users.noreply.github.com>
2016-02-28 02:31:33 +0100

    Update namespace

--- test/demo.php
+++ test/demo.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 echo "<pre>\n";
 
-$luhn = new \Molengo\Luhn();
+$luhn = new \Odan\Luhn\Luhn();
 var_dump($luhn->create('1982'));       // 8
 var_dump($luhn->validate('19828'));    // true
 var_dump($luhn->validate('19829'));    // false
